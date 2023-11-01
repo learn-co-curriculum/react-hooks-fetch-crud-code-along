4ed6d5f231e9605971979c6d37b45dddae5738bb
Shaddy <shadybett540@gmail.com>
2023-11-01 11:57:06 +0300

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem }) {
+
+  function handleAddToCartClick(){
+   fetch(`http://localhost:4000/items/${item.id}`,{
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      isInCart: !item.isInCart,
+    }),
+   }).then((r)=> r.json())
+   .then((updatedItem) => onUpdateItem(updatedItem));
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"}
+      onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
